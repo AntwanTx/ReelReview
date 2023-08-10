09e6a96313e7fdeb54fc964933b96bf9615490c0
AndreTaq <andretaqian@gmail.com>
2023-08-09 20:50:28 -0700

    added NavBar.js

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function NavBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" id="navbar">
+      <Container>
+        <Navbar.Brand href="#home" id="reelReview">ReelReview</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Superhero</Nav.Link>
+            <Nav.Link href="#link">SciFi</Nav.Link>
+            <Nav.Link href="#home">Drama</Nav.Link>
+            <Nav.Link href="#home">Comedy</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
