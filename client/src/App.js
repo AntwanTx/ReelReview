395aaa1d2207ada0dd6cfbbdf0796147ec16caca
alyStrom5062 <Alyenastrom00@gmail.com>
2023-08-12 14:48:09 -0700

    Began react router dom (Aly)

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom'
 import HomePage from "./components/HomePage";
+import Layout from "./Layout";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      {/* <Router>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/superhero/:id" element={<Superhero />} />
-                <Route path="/sciFi/:id" element={<SciFi />} />
-                <Route path="/drama/:id" element={<Drama />} />
-                <Route path="/comedy/:id" element={<Comedy />} />
-              </Routes>
-            </Router> */}
-
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -13,46 +13,48 @@ import psycho from "../movie-posters/american-psycho.jpg";
 
 export default function HomePage(){
     return(
-    <main className="home">
-        <div className="film avengers">
-        <img src={avengers} alt="Poster for Avengers."></img>
-        <h3>Avengers</h3>
-        </div>
+    <>
+        <main className="home">
+            <div className="film avengers">
+            <img src={avengers} alt="Poster for Avengers."></img>
+            <h3>Avengers</h3>
+            </div>
 
-        <div className="film barbie">
-        <img src={barbie} alt="Poster for Barbie."></img>
-        <h3>Barbie</h3>
-        </div>
+            <div className="film barbie">
+            <img src={barbie} alt="Poster for Barbie."></img>
+            <h3>Barbie</h3>
+            </div>
 
-        <div className="film knives">
-        <img src={knives} alt="Poster for Knives Out."></img>
-        <h3>Knives Out</h3>
-        </div>
+            <div className="film knives">
+            <img src={knives} alt="Poster for Knives Out."></img>
+            <h3>Knives Out</h3>
+            </div>
 
-        <div className="film starwars">
-        <img src={starwars} alt="Poster for Star Wars: A New Hope."></img>
-        <h3>Star Wars: A New Hope</h3>
-        </div>
+            <div className="film starwars">
+            <img src={starwars} alt="Poster for Star Wars: A New Hope."></img>
+            <h3>Star Wars: A New Hope</h3>
+            </div>
 
-        <div className="film batman">
-        <img src={batman} alt="Poster for Batman: The Dark Knight."></img>
-        <h3>Batman: The Dark Knight</h3>
-        </div>
+            <div className="film batman">
+            <img src={batman} alt="Poster for Batman: The Dark Knight."></img>
+            <h3>Batman: The Dark Knight</h3>
+            </div>
 
-        <div className="film spiderman">
-        <img src={spiderman} alt="Poster for Spiderman: No Way Home."></img>
-        <h3>Spiderman: No Way Home</h3>
-        </div>
+            <div className="film spiderman">
+            <img src={spiderman} alt="Poster for Spiderman: No Way Home."></img>
+            <h3>Spiderman: No Way Home</h3>
+            </div>
 
-        <div className="film parasite">
-        <img src={parasite}  alt="Poster for Parasite."></img>
-        <h3>Parasite</h3>
-        </div>
+            <div className="film parasite">
+            <img src={parasite}  alt="Poster for Parasite."></img>
+            <h3>Parasite</h3>
+            </div>
 
-        <div className="film psycho">
-        <img src={psycho} alt="Poster for American Psycho."></img>
-        <h3>American Psycho</h3>
-        </div>
-    </main>
+            <div className="film psycho">
+            <img src={psycho} alt="Poster for American Psycho."></img>
+            <h3>American Psycho</h3>
+            </div>
+        </main>
+    </>
     );
 };
