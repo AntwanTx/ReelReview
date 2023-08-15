5a701df1d9eb9379aec895a632260d0f8b6026da
AndreTaq <andretaqian@gmail.com>
2023-08-14 20:14:09 -0700

    updated links in the nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 
 function NavBar() {
@@ -12,10 +12,10 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#superhero">Superhero</Nav.Link>
-            <Nav.Link href="#sciFi">SciFi</Nav.Link>
-            <Nav.Link href="#drama">Drama</Nav.Link>
-            <Nav.Link href="#comedy">Comedy</Nav.Link>
+            <Nav.Link as={Link} to="/superhero">Superhero</Nav.Link>
+            <Nav.Link as={Link} to="/sciFi">SciFi</Nav.Link>
+            <Nav.Link as={Link} to="/drama">Drama</Nav.Link>
+            <Nav.Link as={Link} to="/comedy">Comedy</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
