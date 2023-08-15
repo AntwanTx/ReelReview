6c56bbe92a5c341473d3babfbd9c3db4388497cd
AntwanTx <amunoz021@gmail.com>
2023-08-14 20:51:55 -0500

    Added pages for navbar links

---
+++ client/src/components/Drama.js
@@ -0,0 +1,26 @@
+// Content for Drama
+import React from "react";
+import Navbar from 'Navbar';
+
+import parasite from "../movie-posters/parasite.jpg";
+import psycho from "../movie-posters/american-psycho.jpg";
+
+
+function Drama() {
+    return (
+        <main className="home">
+            <div className="film parasite">
+                <img src={parasite} alt="Poster for Parasite."></img>
+                <h3>Parasite</h3>
+            </div>
+
+            <div className="film psycho">
+                <img src={psycho} alt="Poster for American Psycho."></img>
+                <h3>American Psycho</h3>
+            </div>
+
+        </main>
+    );
+};
+
+export default Drama;
