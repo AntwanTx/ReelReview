140eeec469585270e0c473aeda17312ad719a6b8
alyStrom5062 <Alyenastrom00@gmail.com>
2023-08-18 13:02:31 -0700

    fixed dual index files

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require ('express');
 const app = express();
-const { moviedata } = require('./data/moviedata');
 
-app.use(cors());
-
-app.get('/api/movies', (req, res) => {
-    res.json(moviedata);
+app.get('/test', (req, res) => {
+    res.json('test ok!!!')
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+app.listen(4000);
